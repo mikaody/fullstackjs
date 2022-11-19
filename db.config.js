632b990ee31edf5a1191bd1d11b62c857f30468d
enock <enockrazakamanana@gmail.com>
2022-11-19 18:58:05 +0300

    backend: fix disabled mongo

--- db.config.js
+++ db.config.js
@@ -1,7 +1,7 @@
 /***********************************/
 /*** Import des module nécessaires */
 const { MongoClient } = require('mongodb');
-const URLDB = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017'
+const URLDB = process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://eshop-user:DHXMdyKAHAlyBGa9@cluster0.nywxf.mongodb.net/?retryWrites=true&w=majority'
 
 const client = new MongoClient(URLDB);
 exports.client = client
